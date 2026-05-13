620eef2156e98b120aacd746328679b006834f4b
Suryaa-k <suryaanish1149198@gmail.com>
2026-05-13 13:01:56 +0530

    Javascript

---
+++ Java Script/script.js
@@ -0,0 +1,9 @@
+var num1=10;
+var num2=20;
+console.log(num1+num2);
+
+if (num1>num2){
+    console.log("num1 is greater than num2");
+}else if(num1<num2){
+    console.log("num1 is less than num2");
+}
