50356d180c8ce005cc02e3a2d7a4060fc209e2e4
Suryaa-k <suryaanish1149198@gmail.com>
2026-05-19 11:25:20 +0530

    DOM

---
+++ Java Script/Day 03/script.js
@@ -0,0 +1,11 @@
+// let buchi = document.getElementsByClassName("buchi");
+// console.log(buchi[0]);
+
+// let boo = document.getElementById("boo");
+// boo.innerHTML = "<u>hello</u>";
+
+let paras = document.getElementsByTagName("p");
+
+paras[4].innerHTML = "This is Hello";
+
+document.getElementsByClassName("para")[0].innerHTML = "Welcome "; 
